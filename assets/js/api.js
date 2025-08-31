718e40db21f11faf4ca625aaf24b49108852038d
Gusta-2024git config --global user.email <gustavo.h.n.godoi@gmail.com>
2025-08-31 18:11:45 -0300

    alteração

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,9 @@
 async function fetchProfileData() {
-  const url = "https://gusta-2024.github.io/js-developer-portfolio-2-main/meuPerfil.json";
+  const url = "https://gusta-2024.github.io/Gusta_25.github.io/data/meupessoal.json";
   const response = await fetch(url);
+  if (!response.ok) throw new Error(`Erro ao carregar JSON: ${response.status}`);
   const profileData = await response.json();
   return profileData;
 }
+
 
